9d09d14cc1cf6774d872882ac40700b891de733f
nikthesnick <nikhil.sanjiv.nayyar@gmail.com>
2022-02-22 14:19:06 -0500

    replaced custom qs with std lib sq

--- c1/m1/stats.c
+++ c1/m1/stats.c
@@ -20,34 +20,41 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "stats.h"
 
+int cmpfunc(const void *a, const void *b);
+
 /* Size of the Data Set */
-#define SIZE (5)
-
-void qs(char* arr, int lo, int hi);
-
-void swap(char* arr, int a, int b);
-
-int partition(char* arr, int lo, int hi);
-
+#define SIZE (40)
 
 void main() {
 
-  unsigned char test[5] = {5,2,1,4,3};
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
 
   print_array(test, SIZE);
 
+  // Must sort array to ensure functions work properly
   sort_array(test, SIZE);
 
-  print_array(test,SIZE);
+  print_statistics(test);
 
 }
 
 /* Add other Implementation File Code Here */
 
 void print_statistics(char* arr){
+
+   printf("Mean   : %hhu\n", find_mean(arr,SIZE));
+   printf("Median : %hhu\n", find_median(arr,SIZE));
+   printf("Minimum: %hhu\n", find_minimum(arr,SIZE));
+   printf("Maximum: %hhu\n", find_maximum(arr,SIZE));
+
 }
 
 void print_array(char* arr, int len){
@@ -57,64 +64,59 @@ void print_array(char* arr, int len){
 }
 
 int find_median(char* arr, int len){
-    return 0;
+
+     // Calculates index at middle of array
+     int m = len / 2;
+
+     // returns value at median index
+     return arr[m];
 }
 
 int find_mean(char* arr, int len){
-    return 0;
+    
+    int m = 0;
+    
+    // iterates through values and gets cum sum
+    for(int i = 0; i < len; i++){
+         m = m + arr[i];
+    }
+ 
+    // divides cum sum by total num to get avg
+    m = m / len;
+
+    return m;
 }
 
 int find_maximum(char* arr, int len){
-    return 0;
+    // returns value at greatest end of array
+    return arr[0];
 }
 
 int find_minimum(char* arr, int len){
-    return 0;
+    // retruns value at smallest end of array
+    return arr[len-1];
 }
 
 void sort_array(char* arr, int len){
-    qs(arr, 0,SIZE);
+   // makes call to std library quicksort function
+   qsort(arr,len,1,cmpfunc);
 }
 
-void qs(char* arr, int lo, int hi){
 
-   if(lo >= hi){return;}
+// Function used in qsort algorithm
+int cmpfunc(const void *a, const void *b){
+	unsigned char arg1 = *(const char*)a;
+	unsigned char arg2 = *(const char*)b;
 
-   int p = partition(arr,lo,hi);
-   print_array(arr,SIZE);
-   sleep(3);
-   qs(arr,lo,p);
-   qs(arr,p+1,hi);
+	// Configured s.t. array is greatest - smallest
+	if(arg1 > arg2){
+	  return -1;
+	}
 
+	if(arg1 < arg2){
+	  return 1;
+	}
+
+	return 0;
 }
 
-int partition(char* arr, int lo, int hi){
-
-   int p = hi;
-   int l = lo-1;
-   int r = hi+1;
-
-   while(1){
-   do{
-      l++;
-   }
-   while(arr[l] < arr[p]);
-
-   do{
-      r--;
-   }
-   while(arr[r] > arr[p]);
-
-   if(l>=r){
-      return r;
-   }
-   swap(arr,l,r);
-   }
-
-}
-
-void swap(char* arr, int a, int b){
-	int t = arr[a];
-	arr[a] = arr[b];
-	arr[b] = t;
-}
