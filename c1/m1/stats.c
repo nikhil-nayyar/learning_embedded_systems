bf343974f18430c47b5eedd1248bd3d4cbe2c7b1
nikthesnick <nikhil.sanjiv.nayyar@gmail.com>
2022-02-22 13:38:20 -0500

    Quicksort attempt

--- c1/m1/stats.c
+++ c1/m1/stats.c
@@ -20,23 +20,28 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
 #include "stats.h"
 
 /* Size of the Data Set */
-#define SIZE (40)
+#define SIZE (5)
+
+void qs(char* arr, int lo, int hi);
+
+void swap(char* arr, int a, int b);
+
+int partition(char* arr, int lo, int hi);
+
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+  unsigned char test[5] = {5,2,1,4,3};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  print_array(test, SIZE);
 
-  printf("Joe Biden");
+  sort_array(test, SIZE);
+
+  print_array(test,SIZE);
 
 }
 
@@ -46,6 +51,9 @@ void print_statistics(char* arr){
 }
 
 void print_array(char* arr, int len){
+   for(int i = 0; i < len; i++){
+      printf("%hhu ",arr[i]);
+   } printf("\n");
 }
 
 int find_median(char* arr, int len){
@@ -64,7 +72,49 @@ int find_minimum(char* arr, int len){
     return 0;
 }
 
-void sort_array(char* arr, int lne){
+void sort_array(char* arr, int len){
+    qs(arr, 0,SIZE);
 }
 
+void qs(char* arr, int lo, int hi){
 
+   if(lo >= hi){return;}
+
+   int p = partition(arr,lo,hi);
+   print_array(arr,SIZE);
+   sleep(3);
+   qs(arr,lo,p);
+   qs(arr,p+1,hi);
+
+}
+
+int partition(char* arr, int lo, int hi){
+
+   int p = hi;
+   int l = lo-1;
+   int r = hi+1;
+
+   while(1){
+   do{
+      l++;
+   }
+   while(arr[l] < arr[p]);
+
+   do{
+      r--;
+   }
+   while(arr[r] > arr[p]);
+
+   if(l>=r){
+      return r;
+   }
+   swap(arr,l,r);
+   }
+
+}
+
+void swap(char* arr, int a, int b){
+	int t = arr[a];
+	arr[a] = arr[b];
+	arr[b] = t;
+}
